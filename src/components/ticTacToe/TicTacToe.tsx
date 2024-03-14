bffb3ba4f5807a27778d3e2b563b50946cc23456
Andrey-Fedosov <andrefedosov@gmail.com>
2024-03-15 01:15:32 +0300

    feature: add logic of choosing winter

--- src/components/ticTacToe/TicTacToe.tsx
+++ src/components/ticTacToe/TicTacToe.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 function Square(props: { value: any; onClick: () => void }) {
@@ -12,15 +12,56 @@ function Square(props: { value: any; onClick: () => void }) {
 export function TicTacToe() {
   const [squares, setSquares] = useState(Array(9).fill(""));
   const [isXTurn, setIsXturn] = useState(true);
+  const [status, setStatus] = useState("");
+
+  function getWinner(squares: any): any {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2],
+    ];
+
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [x, y, z] = winningPatterns[i];
+      if (
+        squares[x] &&
+        squares[x] === squares[y] &&
+        squares[x] === squares[z]
+      ) {
+        return squares[x];
+      }
+    }
+    return null;
+  }
 
   function handleClick(currentSquare: any): any {
     let copySquares = [...squares];
-    if (copySquares[currentSquare]) return;
+    if (getWinner(copySquares) || copySquares[currentSquare]) return;
     copySquares[currentSquare] = isXTurn ? "X" : "O";
     setIsXturn(!isXTurn);
     setSquares(copySquares);
     console.log(squares);
   }
+  function restartHandle() {
+    setSquares(Array(9).fill(""));
+    setIsXturn(true);
+    setStatus("");
+  }
+
+  useEffect(() => {
+    if (!getWinner(squares) && squares.every((el) => el !== "")) {
+      setStatus("This is a draw! Please restart");
+    } else if (getWinner(squares)) {
+      setStatus(`Winner is ${getWinner(squares)}. Please restart`);
+    } else {
+      setStatus(`Next player is: ${isXTurn ? "X" : "O"} `);
+    }
+  }, [squares, isXTurn]);
 
   return (
     <div className="tic-tac-toe-container">
@@ -39,6 +80,8 @@ export function TicTacToe() {
         <Square value={squares[7]} onClick={() => handleClick(7)} />
         <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
+      <h1>{status}</h1>
+      <button onClick={restartHandle}>Restart</button>
     </div>
   );
 }
